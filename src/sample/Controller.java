be282315dc9be9a77bc14c4e36feb6f166799c9f
alanli <alli19@scienceleadership.org>
2018-10-22 14:46:55 -0400

    Edit.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
 public class Controller {
+    public Label theQuestion;
+    public RadioButton button1;
+    public RadioButton button2;
+    public RadioButton button3;
+    public RadioButton button4;
+
+    public void questionAnswered() {
+        System.out.println("A question has been answered.");
+        theQuestion.setText("Here is another question...");
+        button1.setText("Pretzel");
+
+    }
 }

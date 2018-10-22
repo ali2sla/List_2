4c09991722522d12eae6719d43c9528dce705b1e
alanli <alli19@scienceleadership.org>
2018-10-22 15:04:31 -0400

    Edit.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,16 @@
 package sample;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.RadioButton;
 
 public class Controller {
     public Label theQuestion;
-    public RadioButton button1;
-    public RadioButton button2;
-    public RadioButton button3;
-    public RadioButton button4;
+    public RadioButton button1, button2, button3, button4;
+
+    ToggleGroup answer;
+
+    public void setTheQuestion;
 
     public void questionAnswered() {
         System.out.println("A question has been answered.");
